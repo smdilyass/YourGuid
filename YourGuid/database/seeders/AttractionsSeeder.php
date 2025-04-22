d8b50dcfa30a160130f43ffb838ac30b4f83622b
smdilyass <smdilyass@gmail.com>
2025-04-22 11:50:58 +0100

    Add seeders for User, Attractions, Categories, CategoryItems, Contacts, and Events

---
+++ YourGuid/database/seeders/AttractionsSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AttractionsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
