72f16cbe403522f173c51feec9443560405138d5
smdilyass <smdilyass@gmail.com>
2025-04-22 11:44:26 +0100

    I Add StadiumsController with resource methods for managing stadiums

---
+++ YourGuid/app/Http/Controllers/StadiumsController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\stadiums;
+use App\Http\Requests\StorestadiumsRequest;
+use App\Http\Requests\UpdatestadiumsRequest;
+
+class StadiumsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorestadiumsRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(stadiums $stadiums)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(stadiums $stadiums)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatestadiumsRequest $request, stadiums $stadiums)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(stadiums $stadiums)
+    {
+        //
+    }
+}
