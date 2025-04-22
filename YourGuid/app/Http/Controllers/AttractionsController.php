e24449d7d68e09331319f355aa4b94cae2acf9f7
smdilyass <smdilyass@gmail.com>
2025-04-22 11:40:16 +0100

    I Add AttractionsController with resource methods for managing attractions

---
+++ YourGuid/app/Http/Controllers/AttractionsController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\attractions;
+use App\Http\Requests\StoreattractionsRequest;
+use App\Http\Requests\UpdateattractionsRequest;
+
+class AttractionsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreattractionsRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(attractions $attractions)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(attractions $attractions)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateattractionsRequest $request, attractions $attractions)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(attractions $attractions)
+    {
+        //
+    }
+}
