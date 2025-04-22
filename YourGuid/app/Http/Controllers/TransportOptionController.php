09dcf7981b80b37c2f0615b6a6fb9aa37913d036
smdilyass <smdilyass@gmail.com>
2025-04-22 11:44:58 +0100

    I Add TransportOptionController with resource methods for managing transport options

---
+++ YourGuid/app/Http/Controllers/TransportOptionController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\transportOption;
+use App\Http\Requests\StoretransportOptionRequest;
+use App\Http\Requests\UpdatetransportOptionRequest;
+
+class TransportOptionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoretransportOptionRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(transportOption $transportOption)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(transportOption $transportOption)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatetransportOptionRequest $request, transportOption $transportOption)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(transportOption $transportOption)
+    {
+        //
+    }
+}
