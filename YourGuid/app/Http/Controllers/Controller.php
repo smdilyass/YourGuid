01f3014a22a12260dc5461074f58a2178efd8d0c
smdilyass <smdilyass@gmail.com>
2025-04-27 21:23:12 +0100

    I Implement show method to retrieve and display dishes in Controller

--- YourGuid/app/Http/Controllers/Controller.php
+++ YourGuid/app/Http/Controllers/Controller.php
@@ -4,5 +4,9 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    public function show()
+{
+    $dishes = Dish::all(); // Or any other query to get your dishes
+    return view('culture', compact('dishes'));
+}
 }
