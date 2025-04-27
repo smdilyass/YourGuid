b3cac90291def0997d565c17dd73a74e2a5efc41
smdilyass <smdilyass@gmail.com>
2025-04-27 21:24:38 +0100

    I Add example hotel locations data to index method in HotelsController

--- YourGuid/app/Http/Controllers/HotelsController.php
+++ YourGuid/app/Http/Controllers/HotelsController.php
@@ -13,7 +13,17 @@ class HotelsController extends Controller
      */
     public function index()
     {
-        //
+        // Example hotel locations data
+        $hotels = [
+            ['name' => 'Hotel Casablanca', 'city' => 'Casablanca', 'lat' => 33.589, 'lng' => -7.611],
+            ['name' => 'Hotel Marrakech', 'city' => 'Marrakech', 'lat' => 31.627, 'lng' => -8.034],
+            ['name' => 'Hotel Rabat', 'city' => 'Rabat', 'lat' => 34.021, 'lng' => -6.842],
+            ['name' => 'Hotel Tangier', 'city' => 'Tangier', 'lat' => 35.748, 'lng' => -5.834],
+            ['name' => 'Hotel Agadir', 'city' => 'Agadir', 'lat' => 30.393, 'lng' => -9.548],
+            ['name' => 'Hotel Fes', 'city' => 'Fes', 'lat' => 34.027, 'lng' => -5.008],
+        ];
+
+        return view('categories.hotels', ['hotels' => $hotels]);
     }
 
     /**
