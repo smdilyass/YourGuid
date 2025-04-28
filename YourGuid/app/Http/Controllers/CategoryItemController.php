4042be83f65f722aafbdb65c884dc43862238a9c
smdilyass <smdilyass@gmail.com>
2025-04-28 12:05:19 +0100

    I Refactor CategoryItemController to handle category-specific item retrieval and update view paths

--- YourGuid/app/Http/Controllers/CategoryItemController.php
+++ YourGuid/app/Http/Controllers/CategoryItemController.php
@@ -6,7 +6,7 @@ use App\Models\CategoryItem;
 use App\Http\Requests\StoreCategoryItemRequest;
 use App\Http\Requests\UpdateCategoryItemRequest;
 
-use App\Models\CategoryItem;
+
 
 class CategoryItemController extends Controller
 {
@@ -14,25 +14,50 @@ class CategoryItemController extends Controller
      * Display a listing of the resource.
      * Show all category items.
      */
-    public function index()
+    public function index($category)
     {
-        // Get all category items from the database
-        $categoryItems = CategoryItem::all();
+        // Get all category items for the given category from the database
+        $categoryItems = CategoryItem::where('category_id', $category->id)->get();
 
-        // Return the category items view with the data
-        return view('category_items.index', compact('categoryItems'));
+        // Return the category items view with the data and category
+        return view('admin.category-items.index', compact('categoryItems', 'category'));
+    }
+
+    public function create($category)
+    {
+        // Return the create category item view with the category
+        return view('admin.category-items.create', compact('category'));
+    }
+
+    public function update($category, $id)
+    {
+        // Find the category item by id or fail with 404
+        $categoryItem = CategoryItem::findOrFail($id);
+
+        // Return the update category item view with the data and category
+        return view('admin.category-items.update', compact('categoryItem', 'category'));
+    }
+    public function delete($id)
+    {   
+        // Find the category item by id or fail with 404
+        $categoryItem = CategoryItem::findOrFail($id);
+
+        // Delete the category item
+        $categoryItem->delete();
+
+        // Redirect to the category items index page
+        return redirect()->route('category_items.index');
     }
 
     /**
      * Display the specified resource.
      * Show a single category item by id.
      */
-    public function show($id)
-    {
-        // Find the category item by id or fail with 404
-        $categoryItem = CategoryItem::findOrFail($id);
-
-        // Return the category item view with the data
-        return view('category_items.show', compact('categoryItem'));
-    }
+    // public function show($id)
+    // {
+    //     // This method signature is updated to be compatible with base Controller class
+    //     abort(404);
+    //     return view('admin.category-items.show', compact('categoryItem'));
+    // }
+    
 }
